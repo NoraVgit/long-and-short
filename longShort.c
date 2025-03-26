a14667b9f9cc1a6339c00c69150606943773f30c
NoraV <rvanleer@student.sierracollege.edu>
2025-03-25 19:37:31 -0700

    added writing to file as a test

--- longShort.c
+++ longShort.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+int getLength(char line[])//just a get length function
+{
+    int length = 0;
+    while(line[length] != '\n' || line[length] != '\0')//while not the end of line character or new line
+    {
+        length++;
+    }
+    return length;
+}
 int main(int argc, char * argv[])
 {
     if(argc < 4)
@@ -10,6 +18,9 @@ int main(int argc, char * argv[])
         return 1;
     }
     FILE * input = fopen(argv[1],"r");
+    FILE * shortWrite = fopen(argv[2],"w");
+    FILE * longWrite = fopen(argv[3],"w");
+
     if(!input)
     {
         printf("could not open file for reading\n");
@@ -20,7 +31,9 @@ int main(int argc, char * argv[])
 
     while(fgets(line,40,input) != NULL)
     {
-        printf("%s",line);
+        //printf("%s",line);
+        fprintf(shortWrite, "%s", line);
+        
     }
     return 0;
 }
