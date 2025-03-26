0b0e86666f1a233414b3af3fd74ade00a27c065f
NoraV <rvanleer@student.sierracollege.edu>
2025-03-25 19:05:42 -0700

    added conditions to make sure args and file opens

--- longShort.c
+++ longShort.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char * argv[])
 {
-    
-
+    if(argc < 3)
+    {
+        printf("Please enter missing command arguments (read file) (write file) (write file)\n");
+        return 1;
+    }
+    FILE * input = fopen(argv[0],"r");
+    if(!input)
+    {
+        printf("could not open file for reading\n");
+        return 1;
+    }
     return 0;
 }
