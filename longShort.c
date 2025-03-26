7d958569f3a632f900adddaee35f332f64dfd2ac
NoraV <rvanleer@student.sierracollege.edu>
2025-03-25 19:16:29 -0700

    demo just printing file content and fixed two bugs

--- longShort.c
+++ longShort.c
@@ -4,16 +4,23 @@
 
 int main(int argc, char * argv[])
 {
-    if(argc < 3)
+    if(argc < 4)
     {
-        printf("Please enter missing command arguments (read file) (write file) (write file)\n");
+        printf("Please enter missing command arguments: ./a.out (read file) (write file) (write file)\n");
         return 1;
     }
-    FILE * input = fopen(argv[0],"r");
+    FILE * input = fopen(argv[1],"r");
     if(!input)
     {
         printf("could not open file for reading\n");
         return 1;
+    }
+
+    char line[40];//for holding each line of files
+
+    while(fgets(line,40,input) != NULL)
+    {
+        printf("%s",line);
     }
     return 0;
 }
