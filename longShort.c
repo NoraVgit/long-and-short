45a1cd17d333cdf06a7f02d84b28507845c2020c
NoraV <rvanleer@student.sierracollege.edu>
2025-03-25 21:18:35 -0700

    final changes to program

--- longShort.c
+++ longShort.c
@@ -2,44 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
+//coded by Nora Van Leer
 int main(int argc, char * argv[])
 {
-    if(argc < 4)
+    if(argc < 4)//if the user didnt provide enougth arguments the user is informed and program is exited
     {
         printf("Please enter missing command arguments: ./a.out (read file) (write file) (write file)\n");
         return 1;
     }
-    FILE * input = fopen(argv[1],"r");
-    FILE * shortWrite = fopen(argv[2],"w");
-    FILE * longWrite = fopen(argv[3],"w");
+    FILE * input = fopen(argv[1],"r");//opens file to read contents from
+    FILE * shortWrite = fopen(argv[2],"w");//opens the second file for short lines
+    FILE * longWrite = fopen(argv[3],"w");//opens the third file for long lines
 
-    if(!input)
+    if(!input)//if there was an issue opening the file
     {
         printf("could not open file for reading\n");
         return 1;
     }
 
     char line[40];//for holding each line of files
-
-    while(fgets(line,40,input) != NULL)
+    int shortLines = 0;
+    int longLines = 0;
+    while(fgets(line,39,input) != NULL)//gets a line of input from the input file
     {
-        if(strlen(line) < 20)
+        if(strlen(line) < 20)//if the strings length is less than 20 chars
         {
             for(int i = 0; i < strlen(line); i++)
             {
-                fprintf(shortWrite,"%c",toupper(line[i]));
+                fprintf(shortWrite,"%c",toupper(line[i]));//converts to uppcase and prints to the short file
+                
             }
+            shortLines++;//increase the number of shortlines 
         }
         else
         {
             for(int j = 0; j < strlen(line); j++)
             {
-                //printf("%c", tolower(line[j]));
-                fprintf(longWrite,"%c",(line[j]));
+                fprintf(longWrite,"%c",tolower(line[j]));//else converts to lowercase and prints to the long file
+                
             }
+            longLines++;//increase the number of longlines
         }
         
     }
+    printf("%d lines written to %s\n",shortLines,argv[2]);//displays number of short lines to user
+    printf("%d lines written to %s\n",longLines,argv[3]);//displays the number of long lines to user
+    //closes all files
+    fclose(longWrite);
+    fclose(shortWrite);
+    fclose(input);
     return 0;
 }
